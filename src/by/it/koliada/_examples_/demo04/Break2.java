ba1679611a9e826b1610ca5e547d84774517aaff
JustgoodDeal <stostoraz@gmail.com>
2018-11-27 20:59:59 +0400

    lesson05

--- src/by/it/koliada/_examples_/demo04/Break2.java
+++ src/by/it/koliada/_examples_/demo04/Break2.java
@@ -5,10 +5,10 @@ class Break2 {
   public static void main(String args[])   
     throws java.io.IOException { 
  
-    char ch; 
+    char ch;
  
     for( ; ; ) { 
-      ch = (char) System.in.read(); // get a char 
+      ch = (char) System.in.read(); // get a char
       if(ch == 'q') break; 
     } 
     System.out.println("You pressed q!"); 
